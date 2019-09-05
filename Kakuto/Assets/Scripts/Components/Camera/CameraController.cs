8f1ea28a5c210c5cef643a04712da37115e4d83e
David Lonni <david_lonni@hotmail.fr>
2019-09-05 22:59:20 +0200

    Reimport CameraController

---
+++ Kakuto/Assets/Scripts/Components/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float m_SmoothValue = 2.0f;
+
+    private Vector3 m_TargetPosition;
+
+    void Start()
+    {
+        m_TargetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, m_TargetPosition, Time.deltaTime * m_SmoothValue);
+    }
+}
