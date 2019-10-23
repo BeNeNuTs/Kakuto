2bc864a4532968293f79fb5efb10a3b2ff1c6c0f
David Lonni <david_lonni@hotmail.fr>
2019-10-23 22:02:56 +0200

    Frame Counter
    - Added a frame counter

--- Kakuto/Assets/Scripts/UI/FPSDisplay.cs
+++ Kakuto/Assets/Scripts/UI/FPSDisplay.cs
@@ -17,16 +17,33 @@ public class FPSDisplay : MonoBehaviour
         {
             int w = Screen.width, h = Screen.height;
 
-            GUIStyle style = new GUIStyle();
+            // Display FPS ///////////////
+            {
+                GUIStyle style = new GUIStyle
+                {
+                    alignment = TextAnchor.UpperLeft,
+                    fontSize = h * 2 / 100
+                };
+                style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+                float msec = m_DeltaTime * 1000.0f;
+                float fps = 1.0f / m_DeltaTime;
+                string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+                GUI.Label(new Rect(0, 0, w, style.fontSize), text, style);
+            }
+            //////////////////////////////
 
-            Rect rect = new Rect(0, 0, w, h * 2 / 100);
-            style.alignment = TextAnchor.UpperLeft;
-            style.fontSize = h * 2 / 100;
-            style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
-            float msec = m_DeltaTime * 1000.0f;
-            float fps = 1.0f / m_DeltaTime;
-            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-            GUI.Label(rect, text, style);
+            // Display Frame Counter /////
+            {
+                GUIStyle style = new GUIStyle
+                {
+                    alignment = TextAnchor.LowerCenter,
+                    fontSize = h * 3 / 100
+                };
+                style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+                string text = "Frame Counter: " + Time.frameCount;
+                GUI.Label(new Rect(0, h - style.fontSize * 2.0f, w, style.fontSize), text, style);
+            }
+            //////////////////////////////
         }
     }
 }
