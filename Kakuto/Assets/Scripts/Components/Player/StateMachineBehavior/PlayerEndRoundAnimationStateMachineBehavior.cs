8d706a5e06b92ba104bf97ef8034f5c7a93ef721
David Lonni <david_lonni@hotmail.fr>
2020-07-11 17:39:36 +0200

    End of round animations
    - Freeze end of round animations on last frame

--- Kakuto/Assets/Scripts/Components/Player/StateMachineBehavior/PlayerEndRoundAnimationStateMachineBehavior.cs
+++ Kakuto/Assets/Scripts/Components/Player/StateMachineBehavior/PlayerEndRoundAnimationStateMachineBehavior.cs
@@ -2,23 +2,30 @@
 
 public class PlayerEndRoundAnimationStateMachineBehavior : StateMachineBehaviour
 {
+    private bool m_EndOfRoundAnim = false;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_EndOfRoundAnim = false;
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.normalizedTime >= 1.0f && !m_EndOfRoundAnim)
+        {
+            PlayerInfoComponent infoComponent = animator.transform.root.GetComponent<PlayerInfoComponent>();
+            Utils.GetPlayerEventManager<EPlayer>(animator.gameObject).TriggerEvent(EPlayerEvent.EndOfRoundAnimation, infoComponent.GetPlayerEnum());
+            m_EndOfRoundAnim = true;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
     //    
     //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        PlayerInfoComponent infoComponent = animator.transform.root.GetComponent<PlayerInfoComponent>();
-        Utils.GetPlayerEventManager<EPlayer>(animator.gameObject).TriggerEvent(EPlayerEvent.EndOfRoundAnimation, infoComponent.GetPlayerEnum());
-    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
