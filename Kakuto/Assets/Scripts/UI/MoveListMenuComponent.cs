16183e9571a6e362d69190b40a918f4461c612c8
David Lonni <david_lonni@hotmail.fr>
2021-02-17 21:08:05 +0100

    MoveList scroll view
    - Move scroll view
    - Fix #244

---
+++ Kakuto/Assets/Scripts/UI/MoveListMenuComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveListMenuComponent : MenuComponent
+{
+#pragma warning disable 0649
+    [SerializeField] private ScrollRect m_ScrollRect;
+    [SerializeField] private float m_ScrollSpeed;
+#pragma warning restore 0649
+
+    protected override void OnUpdate_Internal()
+    {
+        base.OnUpdate_Internal();
+        if(GamePadManager.GetJumpInput((int)EPlayer.Player1) || GamePadManager.GetJumpInput((int)EPlayer.Player2))
+        {
+            UpdateScrollView(false);
+        }
+        else if (GamePadManager.GetCrouchInput((int)EPlayer.Player1) || GamePadManager.GetCrouchInput((int)EPlayer.Player2))
+        {
+            UpdateScrollView(true);
+        }
+    }
+
+    private void UpdateScrollView(bool moveUp)
+    {
+        if(moveUp)
+            m_ScrollRect.verticalNormalizedPosition += m_ScrollSpeed;
+        else
+            m_ScrollRect.verticalNormalizedPosition -= m_ScrollSpeed;
+    }
+}
