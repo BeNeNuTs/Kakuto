dc61e69c9b99a6ea25cbf8d81e42cd0a72ed9994
David Lonni <david_lonni@hotmail.fr>
2021-04-24 19:39:24 +0200

    AudioAssetImporter
    - Auto setup audio clip compression type and loading type according if it's a music or not

---
+++ Kakuto/Assets/Editor/AudioAssetImporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class AudioAssetPostProcessor : AssetPostprocessor
+{
+    void OnPreprocessAudio()
+    {
+        AudioImporter audioImporter = (AudioImporter)assetImporter;
+        audioImporter.forceToMono = true;
+        AudioImporterSampleSettings settings = audioImporter.defaultSampleSettings;
+        bool isMusic = assetPath.Contains("Music");
+        settings.loadType = isMusic ? UnityEngine.AudioClipLoadType.Streaming : UnityEngine.AudioClipLoadType.DecompressOnLoad;
+        settings.compressionFormat = isMusic ? UnityEngine.AudioCompressionFormat.PCM : UnityEngine.AudioCompressionFormat.Vorbis;
+    }
+}
